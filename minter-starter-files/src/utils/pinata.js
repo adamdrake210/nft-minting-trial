fc8c8a985719d2423ff6bdcb798c893f7e2cc097
Adam Drake <addrake@ebay.com>
2021-11-01 22:02:36 +0100

    chore: setting up project and minting functionality

---
+++ minter-starter-files/src/utils/pinata.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const SECRET = process.env.REACT_APP_PINATA_SECRET;
+const KEY = process.env.REACT_APP_PINATA_KEY;
+
+export const pinJSONToIPFS = async (JSONBody) => {
+  const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+  //making axios POST request to Pinata ⬇️
+  return axios
+    .post(url, JSONBody, {
+      headers: {
+        pinata_api_key: KEY,
+        pinata_secret_api_key: SECRET,
+      },
+    })
+    .then(function (response) {
+      return {
+        success: true,
+        pinataUrl:
+          "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash,
+      };
+    })
+    .catch(function (error) {
+      console.log(error);
+      return {
+        success: false,
+        message: error.message,
+      };
+    });
+};
